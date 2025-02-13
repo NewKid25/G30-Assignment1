f31994556791cae23e64ef52897f766cceafc1c0
Ebrasheed <131557712+Ebrasheed@users.noreply.github.com>
2025-02-12 21:52:11 -0600

    public Book searchByISBN(String ISBN)
    This method will search the library for the matching ISBN number and return it

--- src/com/app/Library.java
+++ src/com/app/Library.java
@@ -51,6 +51,23 @@ public class Library {
 		return false;
 	}
 
+	/**
+	 * This method takes ISBN and searches the library for that ISBN
+	 * @param ISBN
+	 * this is the ISBN the method looks for
+	 * @return
+	 * returns the book with the matching ISBN if it matches, returns null if the ISBN does not match
+	 */
+	public Book seacrhByISBN(String ISBN) {
+		for(int i = 0; i < books.length; i++) {
+			if(books[i] != null && books[i].equals(ISBN)) {
+				return books[i];
+			}
+		}
+		System.out.println("The book you are trying to find was not found");
+		return null;
+	}
+
 	
 
 }
