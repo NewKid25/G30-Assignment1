45127abfcc292b5a933e34404c80d609ae4dad30
Ebrasheed <131557712+Ebrasheed@users.noreply.github.com>
2025-02-12 21:50:49 -0600

    public boolean removeBook(Book book)
    This method will remove a book from the array

--- src/com/app/Library.java
+++ src/com/app/Library.java
@@ -8,6 +8,7 @@ public class Library {
 		books = new Book[5];
 		count = 0;
 	}
+	
 	/**
 	 * This method will add a book to the library if there is space
 	 * @param book
@@ -26,6 +27,27 @@ public class Library {
 
 		}
 		System.out.println("You cannot add anymore books");
+		return false;
+	}
+	
+	/**
+	 * This method takes a book, checks the library for the book, and removes the book if it exists
+	 * @param book
+	 * This is the book to be removed
+	 * @return
+	 * Return true if the book was removed. Return false if the book was not removed
+	 */
+	public boolean removeBook(Book book) {
+		for(int i = 0; i < books.length; i++) {
+			if(book != null && books[i].equals(book)) {
+				books[i] = null;
+				count--;
+				System.out.println("The book, "+ book +", was succefully removed");
+				return true;
+			} 
+			
+		}
+		System.out.println("The book, "+ book +", could not be removed");
 		return false;
 	}
 
